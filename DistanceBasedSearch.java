2f8bf7b3d853402188447526c3c0294ebd725f06
Jonathan Bereyziat <jon.jbstar@gmail.com>
2017-11-03 15:32:46 +0100

    Update DistanceBasedSearch.java

--- DistanceBasedSearch.java
+++ DistanceBasedSearch.java
@@ -5,10 +5,8 @@ public class DistanceBasedSearch {
 	/**
 	 * Computes the mean absolute error between two RGB pixels, channel by channel.
 	 * 
-	 * @param patternPixel
-	 *            : a integer, the second RGB pixel.
-	 * @param imagePixel
-	 *            : a integer, the first RGB pixel.
+	 * @param patternPixel: a integer, the second RGB pixel. Requirement: none, any integer
+	 * @param imagePixel: a integer, the first RGB pixel. Requirement: none, any integer
 	 * @return a double, the value of the error for the RGB pixel pair. (an integer
 	 *         in [0, 255])
 	 */
@@ -27,17 +25,14 @@ public class DistanceBasedSearch {
 	 * Computes the mean absolute error loss of a RGB pattern if positioned at the
 	 * provided row, column-coordinates in a RGB image
 	 * 
-	 * @param row
-	 *            : a integer, the row-coordinate of the upper left corner of the
-	 *            pattern in the image.
-	 * @param column
-	 *            : a integer, the column-coordinate of the upper left corner of the
-	 *            pattern in the image.
-	 * @param pattern
-	 *            : an 2D array of integers, the RGB pattern to find
-	 * @param image
-	 *            : an 2D array of integers, the RGB image where to look for the
-	 *            pattern
+	 * @param row: a integer, the row-coordinate of the upper left corner of the pattern in the image. 
+	 * Requirement: row must be greater than or equals to 0 and smaller than the difference of the row number of the image +1 and the row number of the pattern 
+	 * @param column: a integer, the column-coordinate of the upper left corner of the pattern in the image. 
+	 * Requirement: column must be greater than or equal to 0 and smaller than the difference of the column number of the image +1 and the column number of the pattern
+	 * @param pattern: an 2D array of integers, the RGB pattern to find. 
+	 * Requirement: pattern has to be an array greater than 1 by 1
+	 * @param image: an 2D array of integers, the RGB image where to look for the pattern. 
+	 * Requirement: image has to be an array greater than 1 by 1
 	 * @return a double, the mean absolute error
 	 * @return a double, mean absolute error value at position (row, col) between
 	 *         the pattern and the part of the base image that is covered by the
@@ -45,11 +40,16 @@ public class DistanceBasedSearch {
 	 *         the denominator is -1
 	 */
 	public static double meanAbsoluteError(int row, int col, int[][] pattern, int[][] image) {
+		
+		assert pattern.length > 0 && pattern[0].length > 0;
+		assert image.length > 0 && image[0].length > 0;
+		assert row >= 0 && row <= (image.length-pattern.length+1);
+		assert col >= 0 && col <= (image[0].length-pattern[0].length+1);
 
 		double meanAbsoluteError = 0;
 		int nbPixels = pattern.length * pattern[0].length;
 		for (int i = 0; i < pattern.length; i++) {
-			for (int j = 0; j < pattern[i].length; j++) {
+			for (int j = 0; j < pattern[0].length; j++) {
 				meanAbsoluteError += pixelAbsoluteError(pattern[i][j], image[row + i][col + j]);
 			}
 		}
@@ -61,23 +61,27 @@ public class DistanceBasedSearch {
 	/**
 	 * Compute the distanceMatrix between a RGB image and a RGB pattern
 	 * 
-	 * @param pattern
-	 *            : an 2D array of integers, the RGB pattern to find
-	 * @param image
-	 *            : an 2D array of integers, the RGB image where to look for the
-	 *            pattern
+	 * @param pattern: an 2D array of integers, the RGB pattern to find
+	 * Requirement: pattern has to be an array greater than 1 by 1
+	 * @param image: an 2D array of integers, the RGB image where to look for the pattern
+	 * Requirement: image has to be an array greater than 1 by 1
 	 * @return a 2D array of doubles, containing for each pixel of a original RGB
 	 *         image, the distance (meanAbsoluteError) between the image's window
 	 *         and the pattern placed over this pixel (upper-left corner)
 	 */
 	public static double[][] distanceMatrix(int[][] pattern, int[][] image) {
-		double[][] distanceMatrix = new double[image.length - pattern.length + 1][image[0].length - pattern[0].length
-				+ 1];
+		
+		assert pattern.length > 0 && pattern[0].length > 0;
+		assert image.length > 0 && image[0].length > 0;
+		
+		double[][] distanceMatrix = new double[image.length - pattern.length + 1][image[0].length - pattern[0].length+ 1];
 		for (int row = 0; row < distanceMatrix.length; row++) {
-			for (int col = 0; col < distanceMatrix[row].length; col++) {
+			for (int col = 0; col < distanceMatrix[0].length; col++) {
 				distanceMatrix[row][col] = meanAbsoluteError(row, col, pattern, image);
 			}
 		}
 		return distanceMatrix;
 	}
+	
 }
+
